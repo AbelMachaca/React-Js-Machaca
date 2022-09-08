a9c954f90c9993644403df26248432ae6fc6826e
Abel Machaca <abelmachaca99@gmail.com>
2022-09-08 01:19:48 -0300

    subida de NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,36 @@
+// import './navbar.css';
+import './style.css';
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg celeste">
+            <div className="container-fluid">
+                <a className="navbar-brand blanco" href="#">TecnoConectados</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                    <a className="nav-link blanco" aria-current="page" href="#">categoria 1</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link blanco" href="#">categoria 2</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link blanco" href="#">categoria 3</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link blanco">categoria 4</a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav>
+
+    );
+  }
+  
+
+export default Navbar;
+
