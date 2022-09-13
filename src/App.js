95c36303bbb94ccd3d8689aef45ba4e68fe5dc27
Abel Machaca <abelmachaca99@gmail.com>
2022-09-13 15:48:51 -0300

    agregado contador

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
 
 const App = () => {
   return (
     <div>
       <NavBar/>
       <ItemListContainer item="primer itemlist" />
+      <ItemCount initial= {1} stock={5} />
 
     </div>
   );
